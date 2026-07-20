5678e881dcc436adf4aae8e4b0facba182561b17
주형진 <hj06869@naver.com>
2026-07-20 13:25:16 +0900

    UPROPERTY, UFUNCTION, Component추가

--- Source/Unreal_Cpp/Private/TestActor.cpp
+++ Source/Unreal_Cpp/Private/TestActor.cpp
@@ -9,6 +9,11 @@ ATestActor::ATestActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	USceneComponent* Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	MainMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MainMesh"));
+	MainMesh->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +21,15 @@ void ATestActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	GEngine->AddOnScreenDebugMessage(-1, 5.0, FColor::Green, TEXT("언리얼 시작"));
+	UE_LOG(LogTemp, Log, TEXT("BeginPlay : Log"));
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlay : Warning"));
+	UE_LOG(LogTemp, Error, TEXT("BeginPlay : Err"));
+
+	Test_NormalFnuction();
+	Test_UFunction();
+	Test_ImplementableFunc();
+	Test_NativeEnventFunc();
 }
 
 // Called every frame
@@ -23,5 +37,20 @@ void ATestActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	MainMesh->AddRelativeLocation(FVector(0, 0, 1)* MoveSpeed * DeltaTime);
+}
+
+void ATestActor::Test_NormalFnuction()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Test_NormalFnuction : Warning"));
+}
+
+void ATestActor::Test_UFunction()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Test_UFunction : Warning"));
+}
+
+void ATestActor::Test_NativeEnventFunc_Implementation()
+{
 }
 
--- Source/Unreal_Cpp/Public/TestActor.h
+++ Source/Unreal_Cpp/Public/TestActor.h
@@ -22,6 +22,17 @@ protected:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void Test_NormalFnuction();
+
+	UFUNCTION(BlueprintCallable)
+	void Test_UFunction();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void Test_ImplementableFunc();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void Test_NativeEnventFunc();
+
 protected:
 	//C++ 전용 변수
 	//Unreal system에 영향을 받지 않음
@@ -36,4 +47,24 @@ protected:
 	UPROPERTY(VisibleDefaultsOnly)
 	int32 Data2_3 = 23;
 
+	UPROPERTY(EditAnywhere)
+	int32 Data3_1 = 23;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 Data3_2 = 23;
+
+	UPROPERTY(EditInstanceOnly)
+	int32 Data3_3 = 23;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 Data4_1 = 41;
+
+	UPROPERTY(BlueprintReadWrite, Category= "TestActor 변수|Visible")
+	int32 Data4_2 = 42;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UStaticMeshComponent> MainMesh = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MoveSpeed = 10.0f;
 };
